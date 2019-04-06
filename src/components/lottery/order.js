7fd19f5193ede832bda057f44227b21f58fd8337
Carter Cowan <cartercowan@Carters-MacBook-Pro.local>
2019-04-06 18:36:36 -0500

    remove old file

--- src/components/lottery/order.js
+++
@@ -1,9 +0,0 @@
-const names = ['a','b','c'];
-
-const tickets = [];
-for (let index = 0; index < names.length; index++) {
-    const name = names[index];
-    for (let i = 0; i < index + 1 ; i++) {
-        tickets.push = name;
-    }
-}
